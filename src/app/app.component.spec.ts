122557ad34953a96ef7bc1a63f3be090e33261e9
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-09-15 19:49:29 +0530

    Refactored

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Angular-Self-Learning' title`, () => {
+  it(`should have the 'Simple-POS-Angular' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular-Self-Learning');
+    expect(app.title).toEqual('Simple-POS-Angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Angular-Self-Learning');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Simple-POS-Angular');
   });
 });
