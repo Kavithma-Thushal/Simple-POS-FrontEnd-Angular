7b80fe9e07cdb49cd503deaec7e08a88648e5684
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-09-18 19:53:02 +0530

    project name changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Simple-POS-Angular';
+    title = 'Simple-POS-FrontEnd-Angular';
 }
